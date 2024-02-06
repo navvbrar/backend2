8fd4fe223dbd452775ba1462cb56ff0056137146
navvbrar <132538255+navvbrar@users.noreply.github.com>
2024-02-06 16:46:24 -0500

    Your Message

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+const URI= "mongodb+srv://navdeepsinghmkt0:4jqf05lZDFwlArzF@cluster1.rfj14h1.mongodb.net/?retryWrites=true&w=majority"
 const connectdb = ()=>{
- mongoose.connect("mongodb://127.0.0.1/ecommerce2").then(()=>{
+ mongoose.connect(URI).then(()=>{
     console.log("connected to database")
  }).catch((err)=>{
   console.log(err)
