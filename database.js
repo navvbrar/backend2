0fce3b761d534a9abfe325a7785d5a5e0e342ddb
navvbrar <132538255+navvbrar@users.noreply.github.com>
2024-02-08 11:46:45 -0500

    Update database.js updated function with async await

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const URI= "mongodb+srv://navdeepsinghmkt0:4jqf05lZDFwlArzF@cluster1.rfj14h1.mongodb.net/?retryWrites=true&w=majority"
-const connectdb = ()=>{
- mongoose.connect(URI).then(()=>{
+const connectdb = async()=>{
+await mongoose.connect(URI).then(()=>{
     console.log("connected to database")
  }).catch((err)=>{
   console.log(err)
