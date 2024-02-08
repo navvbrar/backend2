0f7203d4a728cbb3a1c48d9961809eec5d8622c3
navvbrar <132538255+navvbrar@users.noreply.github.com>
2024-02-08 12:10:37 -0500

    Update database.js updated mongoose.connect parameters

--- database.js
+++ database.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 const URI= "mongodb+srv://navdeepsinghmkt0:4jqf05lZDFwlArzF@cluster1.rfj14h1.mongodb.net/?retryWrites=true&w=majority"
 const connectdb = async()=>{
-await mongoose.connect(URI).then(()=>{
+await mongoose.connect(URI,{
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(()=>{
     console.log("connected to database")
  }).catch((err)=>{
   console.log(err)
