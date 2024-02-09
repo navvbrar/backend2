064714ed8fe7459f5f64f0530376568ce281cb3c
navvbrar <132538255+navvbrar@users.noreply.github.com>
2024-02-09 11:18:03 -0500

    added auth

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const URI= "mongodb+srv://navdeepsinghmkt0:4jqf05lZDFwlArzF@cluster1.rfj14h1.mongodb.net/?retryWrites=true&w=majority"
-const connectdb = ()=>{
- mongoose.connect(URI).then(()=>{
-    console.log("connected to database")
+const connectdb = async()=>{
+ await mongoose.connect(URI).then(()=>{
+  console.log("connected to database")
  }).catch((err)=>{
   console.log(err)
  })
